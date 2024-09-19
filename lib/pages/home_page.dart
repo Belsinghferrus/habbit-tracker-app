20d449036c2621577b5cef4ed4d9d39dc590b193
Belsinghferrus <ferrusbelsingh@gmail.com>
2024-09-19 19:16:07 +0530

    dialog box added

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,5 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:habbit_tracker/theme/theme_provider.dart';
-import 'package:provider/provider.dart';
+import 'package:habbit_tracker/components/my_drawer.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -11,21 +9,57 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  TextEditingController habitTextController = TextEditingController();
+
+  //create new habit
+  void createNewHabit() {
+    showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+              title: Text("Create New Habit"),
+              content: TextField(
+                autocorrect: true,
+                controller: habitTextController,
+                decoration: const InputDecoration(
+                    hintText: "Wake up at 6:00 am",
+                    border: OutlineInputBorder()),
+              ),
+              actions: [
+                //save
+                MaterialButton(onPressed: () {
+                  // get new habit name
+
+                  //save to db
+
+                  //pop box
+
+                  //clear control
+                }),
+                //cancel
+                MaterialButton(
+                    child: const Text("Cancel"),
+                    onPressed: () {
+                      habitTextController.clear();
+                      Navigator.of(context).pop();
+                    })
+              ],
+            ));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
       appBar: AppBar(
         elevation: 0,
       ),
-      drawer: Drawer(
-        backgroundColor: Theme.of(context).colorScheme.background,
-        child: Center(
-          child: CupertinoSwitch(
-            value: Provider.of<ThemeProvider>(context).isDarkMode,
-            onChanged: (value) =>
-                Provider.of<ThemeProvider>(context, listen: false)
-                    .toggleTheme(),
-          ),
+      drawer: const MyDrawer(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: createNewHabit,
+        elevation: 0,
+        backgroundColor: Theme.of(context).colorScheme.tertiary,
+        child: const Icon(
+          Icons.add,
         ),
       ),
     );
